4dfeb0146ea32602cac95cd710bc6425b8bf321e
caleb mccarthy <caleb.a.mccarthy@gmail.com>
2020-12-05 22:17:01 +0000

    Forgot json tag on data object

--- models/user.go
+++ models/user.go
@@ -4,5 +4,5 @@ type User struct {
 	Id       string      `json:"id"`
 	Username string      `json:"username"`
 	Email    string      `json:"email"`
-	Data     interface{} //Unstructured jsonb data. What ever someone wants to store.
+	Data     interface{} `json:"data"` //Unstructured jsonb data. What ever someone wants to store.
 }
